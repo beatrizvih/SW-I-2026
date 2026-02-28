645c6c217aeb7a6fa73e45cdefab35b3b0abf128
Beatriz vitória <beatrizvitoria070510@gmail.com>
2026-02-28 15:00:39 -0300

    Add files via upload

---
+++ aula02/exemplo.php
@@ -0,0 +1,22 @@
+<?php
+
+    $a = 4;
+    $b = 0;
+
+    if ($b == 0 ) {
+        echo "Erro: não é possível dividir por zero.";
+
+    } else {
+
+    $s = $a / $b;
+    echo $s;
+    echo "<br>";
+
+    }
+    
+
+
+    echo "Aqui continua meu programa....";
+
+
+?>
---
+++ aula02/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 02</title>
+</head>
+<body>
+    
+    <h1>AULA 02 - PHP + HTML</h1>
+    <?php
+    
+        $nome = "Emilly";
+        echo "<p style=\"color:purple;\">Bem vindo, $nome!</p>";
+        //echo "<p span style='color:blue'>Bem vindo," . $nome . "</p>";
+
+        echo "<ul>";
+        for ($i=1; $i <= 5 ; $i++) { 
+            echo " <li>item $i</li>";
+            echo "<br>";
+        }
+        echo "</ul>"
+
+    ?>
+
+    
+ 
+
+</body>
+</html>
